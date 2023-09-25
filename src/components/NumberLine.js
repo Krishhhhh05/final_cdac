e3b63d2d8dd360bfadf13915fee1f41e9c678257
candelatesla <candelatesla@gmail.com>
2023-09-25 17:04:12 +0530

    foundit

--- src/components/NumberLine.js
+++ src/components/NumberLine.js
@@ -4,11 +4,13 @@ import createPlotlyComponent from 'react-plotly.js/factory';
 
 const Plot = createPlotlyComponent(Plotly);
 
-const NumberLine = ({ onNext }) => {
+const NumberLine = () => {
+  // Generate x-values from -10 to 10
   const xValues = Array.from({ length: 21 }, (_, index) => index - 10);
 
-  const [highlightedValue, setHighlightedValue] = useState(null);
+  const [highlightedValue, setHighlightedValue] = useState(0);
 
+  // Function to handle user input and highlight the value
   const handleInputChange = (event) => {
     const inputValue = parseFloat(event.target.value);
     if (!isNaN(inputValue) && inputValue >= -10 && inputValue <= 10) {
@@ -18,29 +20,82 @@ const NumberLine = ({ onNext }) => {
     }
   };
 
-  const handleNextClick = () => {
-    if (highlightedValue !== null) {
-      onNext();
-    } else {
-      alert('Please enter a valid value before proceeding.');
-    }
-  };
-
+  // Calculate indices of points to the left and right of the highlighted value
   const highlightIndex = xValues.indexOf(highlightedValue);
   const leftIndices = xValues.slice(0, highlightIndex);
   const rightIndices = xValues.slice(highlightIndex + 1);
 
-  const data = [
-    // ... (unchanged)
-  ];
+ 
 
+  const data = [
+    {
+      x: leftIndices,
+      y: leftIndices.map(() => 0),
+      mode: 'markers',
+      marker: { size: 10, color: 'red' },
+      name: 'Negative Values',
+    },
+    {
+      x: rightIndices,
+      y: rightIndices.map(() => 0),
+      mode: 'markers',
+      marker: { size: 10, color: 'green' },
+      name: 'Positive Values',
+    },
+    {
+      x: [highlightedValue],
+      y: [0],
+      mode: 'markers',
+      marker: { size: 20, color: 'blue' },
+      name: 'Highlighted Value',
+    },
+  ];
+  
   const graphlayout = {
-    // ... (unchanged)
+    xaxis: {
+      showgrid: false,
+      tickmode: 'array',
+      tickvals: xValues,
+      ticktext: xValues.map(String),
+      zeroline: false, 
+
+    },
+    yaxis: {
+      showgrid: false,
+      zeroline: true,
+      zerolinecolor: 'black',
+      zerolinewidth: 3,
+      showticklabels: false,
+    },
+    height: 250,
+    plot_bgcolor: 'white',
+    legend: {
+      traceorder: 'normal', 
+      font: {
+        family: 'Arial, sans-serif',
+        size: 12,
+        color: 'black',
+      },
+     
+      height: '400',
+    },
   };
+  
+  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 p-3 gap-3">
-      {/* ... (content for the left grid, unchanged) */}
+      <div className="bg-gray-300 p-4 rounded-md">
+        <div className='p-4'>
+          <h4 className='flex justify-center items-center'>Instructions</h4>
+          <p>
+            <ul class="list-disc">
+              <li>Enter any value from the number appearing in the graph in the input box</li>
+              <li>You can now see the poitive and negative values respectively</li>
+            </ul>
+          </p>
+        </div>
+      </div>
       <div className="bg-gray-400 p-4 col-span-2 rounded-md">
         <h4 className='flex justify-center items-center'> Visualizing Positive and Pegative values on a numberline</h4>
         <div className='flex justify-center items-center'>
