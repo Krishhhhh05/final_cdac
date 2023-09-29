c90c2fb824ef20c075b45f2a3b1cc17ddfd0016a
candelatesla <candelatesla@gmail.com>
2023-09-29 16:01:41 +0530

    finalfinal

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -112,7 +112,6 @@ const Quiz = () => {
                         </button>
                     )}
                 </div>
-
                 {showAnswers && (
                     <div className="flex justify-center mt-4">
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setShowAnswers(false)}>
